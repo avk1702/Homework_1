1d745e7cbe492d458902075a22d069c3ea8a1454
avk1702 <avk1702@bk.ru>
2023-02-11 13:41:33 +0500

    решение 2 задачи

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a, b, c, max;
+Console.WriteLine("Введите a:");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите c:");
+c = int.Parse(Console.ReadLine());
+max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
