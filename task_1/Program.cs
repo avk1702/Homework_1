b79737dd4c4a33ea7a892d04a4b3412bf5b7cd36
avk1702 <avk1702@bk.ru>
2023-02-11 12:08:20 +0500

    решение 1 задачи

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a, b, max;
+Console.WriteLine("Введите a:");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+  max = a;
+}
+else
+{
+  max = b;
+}
+Console.Write("max = ");
+Console.Write(max);
