3dcc67aebee5c15de2a4b599ef372d253493510a
avk1702 <avk1702@bk.ru>
2023-02-11 14:08:37 +0500

    решение 3 задачи

---
+++ task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+Console.WriteLine("Введите a:");
+a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+
+{
+  Console.WriteLine("Да, это число четное");
+}
+
+else
+{
+  Console.WriteLine("Нет, это число нечетное");
+}
+
