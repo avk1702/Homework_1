ff5e0c00998c11dc31ec5c7691efd5d97b45f448
avk1702 <avk1702@bk.ru>
2023-02-11 15:22:33 +0500

    решение 4 задачи

---
+++ task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int N;
+Console.Write("Введите N:");
+N = int.Parse(Console.ReadLine());
+
+int index;
+index = 1;
+
+while (index <= N)
+{
+  if (index % 2 == 0)
+  {
+    Console.Write(index + " ");
+  }
+  index = index + 1;
+}
