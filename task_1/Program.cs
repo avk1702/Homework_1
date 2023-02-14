697be1b063a20f3b9bce58dafb13ddb942b039a0
avk1702 <avk1702@bk.ru>
2023-02-14 12:38:35 +0500

    Convert.ToInt32(Console.ReadLine());

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,8 +1,10 @@
 ﻿int a, b, max;
 Console.WriteLine("Введите a:");
 a = int.Parse(Console.ReadLine());
+// or Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите b:");
 b = int.Parse(Console.ReadLine());
+// or Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
   max = a;
